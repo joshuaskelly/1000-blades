a663fdeca5975a877a8178ddb142c8a998357c22
Joshua Skelton <joshua.skelton@gmail.com>
2018-02-03 15:30:17 -0800

    :circus_tent:

---
+++ README.md
@@ -0,0 +1,29 @@
+# [![1000-blades](.media/header.gif)](https://github.com/JoshuaSkelly/1000-blades)
+
+# 1000-blades
+[![License: GPL v3](https://img.shields.io/badge/license-MIT-blue.svg)](./LICENSE) [![Python 3](https://img.shields.io/badge/python-3-blue.svg)](https://www.python.org/)
+
+1000 Blades is a community mod project for the video game Delver. The goal is to procedurally generate 1000 swords.
+
+## Contributing
+You can contribute to the project in several ways:
+
+- *Pixel Art* Handy at pushing pixels? These sword parts won't draw themselves.
+- *Color Palettes* Exquisite taste in curating colors? We have a job for you.
+- *Knowledge* Secret procgen knowledge that would be perfect for this project? Enlighten us!
+
+## Installation
+Subscribe to 1000 Blades on the Steam Workshop!
+
+## Dev Installation
+`$ pip install -r requirements.txt `
+
+## Contributors
+[![Joshua Skelton](https://avatars.githubusercontent.com/u/372642?s=130)](http://github.com/joshuaskelly) |
+---|---
+[Joshua Skelton](http://github.com/joshuaskelly) |
+
+## License
+MIT
+
+See the license for the full text.
---
+++ test.py
@@ -0,0 +1,57 @@
+import glob
+import os
+import random
+
+from PIL import Image
+
+# Set the random seed to have deterministic results.
+random.seed("teddybear")
+
+# Load up individual pieces
+palettes = [os.path.normpath(g) for g in glob.glob('./images/palettes/*.png')]
+grips = [os.path.normpath(g) for g in glob.glob('./images/grips/*.png')]
+pommels = [os.path.normpath(g) for g in glob.glob('./images/pommels/*.png')]
+crossguards = [os.path.normpath(g) for g in glob.glob('./images/crossguards/*.png')]
+blades = [os.path.normpath(g) for g in glob.glob('./images/blades/*.png')]
+
+
+def generate_sword_image():
+    """Generates a sword image from pieces
+
+    Returns:
+        A PIL Image object.
+    """
+
+    # Chose a random set of pieces
+    palette = Image.open(random.choice(palettes), 'r')
+    grip = Image.open(random.choice(grips), 'r')
+    pommel = Image.open(random.choice(pommels), 'r')
+    crossguard = Image.open(random.choice(crossguards), 'r')
+    blade = Image.open(random.choice(blades), 'r')
+
+    # Apply palette
+    for image in (grip, pommel, crossguard, blade):
+        image.putpalette(palette.palette)
+
+    composite = Image.new('RGBA', (32, 32))
+
+    # Paste with mask needs to be RGBA data. Convert() is used to accomplish this.
+    composite.paste(grip)
+    composite.paste(pommel, (0, 0), pommel.convert())
+    composite.paste(blade, (0, 0), blade.convert())
+    composite.paste(crossguard, (0, 0), crossguard.convert())
+
+    return composite
+
+
+sheet_size = 32 * 16, 32 * 64
+sprite_sheet = Image.new('RGBA', sheet_size)
+
+# Build the sprite sheet
+for x in range(0, sheet_size[0], 32):
+    for y in range(0, sheet_size[1], 32):
+        image = generate_sword_image()
+        sprite_sheet.paste(image, (x, y))
+
+# Save the sprite sheet to file
+sprite_sheet.save('out.png')
